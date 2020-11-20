9c820e60bdc55ee1be4bb2fcc31a8dc9a957e524
olabode-dev <muhbrahc1@gmail.com>
2020-11-20 02:05:12 +0100

    changed the delete warning message

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function addItems(e){
 function removeItem(e){
     if(e.target.classList.contains('delete')){
         
-        if(confirm("Are you sure you have gotten the item?"))
+        if(confirm("Selected item will be deleted?"))
         var li =  e.target.parentElement;
         itemList.removeChild(li);
     }
